440d25041f736bfc55dd6b0c1912097f934812d3
Darius Nilforooshan <d.nilforooshan@gmail.com>
2023-06-09 11:15:54 +0100

    SOS button on maps page, now able to share location with close contacts

--- app/src/main/java/com/example/ng/NewMessageSheet.kt
+++ app/src/main/java/com/example/ng/NewMessageSheet.kt
@@ -4,9 +4,11 @@ import android.Manifest
 import android.app.PendingIntent
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Address
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Telephony.Mms.Addr
 import android.provider.Telephony.Sms
 import android.telephony.SmsManager
 import android.text.Editable
@@ -21,10 +23,10 @@ import com.example.ng.databinding.FragmentNewMessageSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
-class NewMessageSheet(var contactItem: ContactItem?, val sentPI: PendingIntent) : BottomSheetDialogFragment() {
+class NewMessageSheet(var contactItem: ContactItem?, val sentPI: PendingIntent, val addresses: List<Address>?) : BottomSheetDialogFragment() {
 
     private lateinit var binding: FragmentNewMessageSheetBinding
-   // private lateinit var contactViewModel: ContactViewModel
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -38,6 +40,20 @@ class NewMessageSheet(var contactItem: ContactItem?, val sentPI: PendingIntent) 
                 sentPI, null)
         }
 
+        binding.butLocation.setOnClickListener {
+            val address =
+                addresses!![0].getAddressLine(0) // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+//            val city = addresses!![0].locality
+//            val state = addresses!![0].adminArea
+//            val country = addresses!![0].countryName
+//            val postalCode = addresses!![0].postalCode
+//            val knownName = addresses!![0].featureName // Only if available else return NULL
+            SmsManager.getDefault().sendTextMessage(contactItem?.num, null,
+                "I am at $address", sentPI, null)
+
+        }
+
+
     }
 
     override fun onCreateView(
@@ -48,36 +64,6 @@ class NewMessageSheet(var contactItem: ContactItem?, val sentPI: PendingIntent) 
         return binding.root
     }
 
-//    fun deleteAction() {
-//        if (contactItem != null) {
-//            contactViewModel.deleteContactItem(contactItem!!)
-//            dismiss()
-//        }
-//    }
-
-
-//    private fun saveAction() {
-//
-//
-//        val name = binding.name.text.toString()
-//        val num = binding.phoneNum.text.toString()
-//        if (contactItem == null) {
-//            val newContact = ContactItem(name, num)
-//            contactViewModel.addContactItem(newContact)
-//
-//
-//        } else {
-//            contactItem!!.name = name
-//            contactItem!!.num = num
-//            contactViewModel.updateContactItem(contactItem!!)
-//
-//        }
-//
-//        binding.name.setText("")
-//        binding.phoneNum.setText("")
-//
-//        dismiss()
-//    }
 
 
 
